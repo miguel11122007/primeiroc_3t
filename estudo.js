a3867c6b3ad20b89cad477beffa0243e08dc478f
miguel11122007 <110105979+miguel11122007@users.noreply.github.com>
2022-09-29 12:47:22 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -1,1 +1,6 @@
+let tabuada 2
 
+function Escreva (){
+    document .write (tabuada + " x 1 = " + (tabuada*1));
+    document.write (tabuada + "x 2 = " + (tabuada*2));
+}document.write (tabuada + "x 2 = " + (tabuada*3));
