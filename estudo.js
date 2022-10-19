0dadc5186be4dbd8a3d66099fb4dd7036bd5dc92
miguel11122007 <110105979+miguel11122007@users.noreply.github.com>
2022-10-19 10:02:55 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -1,43 +1,9 @@
-let tabuada = 5;
 
-function escreva(){
-    document.write(tabuada + " x 1 = " + (tabuada*1) + "<br>");
-    document.write(tabuada + " x 2 = " + (tabuada*2) + "<br>");
-    document.write(tabuada + " x 3 = " + (tabuada*3) + "<br>");
-    document.write(tabuada + " x 4 = " + (tabuada*4) + "<br>");
-    document.write(tabuada + " x 5 = " + (tabuada*5) + "<br>");
-    document.write(tabuada + " x 6 = " + (tabuada*6) + "<br>");
-    document.write(tabuada + " x 7 = " + (tabuada*7) + "<br>");
-    document.write(tabuada + " x 8 = " + (tabuada*8) + "<br>");
-    document.write(tabuada + " x 9 = " + (tabuada*9) + "<br>");
-    document.write(tabuada + " x 10 = " + (tabuada*10) + "<br>");
-}
+function Soma(){
+    let n1 = document.getElementById("v1").value;
+    let n2 = document.getElementById("v2").value;
+    let n3 = document.getElementById("v3").value;
 
-function MinhaTabuada(){
-    for(let i = 1; i <= 10; i++){
-        document.write("tabuada do " + i + "<br>");
-        for(let j = 1; j<= 10; j++){
-            document.write(i + " x " + j + " = " + (j*i) + "<br>");
-        }
-        document.write("<br>");
-    }
-}
-
-function Cubo(){
-    for(let i = 20; i <= 2; i--){
-        document.write("o cubo de "+i+ " é ("+i+" x "+i+" x "+i+" ) =  "+(i*i*i)+"<br>");
-    }
-}
-
-function mat(){
-    let v = document.getElementById("valor").value;
-    let j = document.getElementById("juros").value;
-    let j = document.getElementById("meses").value;
-    let r = 0;
-    for(let i=1; i<=t;i++ ){
-         r = v * (1+(j/100));
-         v = r;
-
-    }
-    document.write("resultado" + r);
+    let r = (Number(n1)) + Number(n2) + Number(n3));
+    
 }
